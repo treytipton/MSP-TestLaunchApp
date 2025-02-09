3d57c29a64185aa8f931dc323c5d1539e97c4f2c
Ryan Milewski <36171248+rmilewski4@users.noreply.github.com>
2025-02-09 14:14:30 -0600

    Add LabJack Support, supports singleton creation on startup, and automatically switches to demo mode if device is not present.

---
+++ LabJackHandleManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LabJack;
+
+namespace Project_FREAK
+{
+    public class LabJackHandleManager
+    {
+        private int _handle = -1;
+        private bool _isDemo = true;
+        private static LabJackHandleManager _instance;
+        private LabJackHandleManager()
+        {
+            InitalizeLabJack();
+        }
+        public static LabJackHandleManager Instance => _instance ??= new LabJackHandleManager(); //singleton, only one can be created.
+
+        private void InitalizeLabJack()
+        {
+            try
+            {
+                LJM.OpenS("ANY", "ANY", "ANY", ref _handle);
+                _isDemo = false;
+            }
+            catch (LJM.LJMException exception)
+            {
+                LJM.OpenS("ANY", "ANY", "LJM_DEMO_MODE", ref _handle); //open in demo mode if device not found
+                _isDemo = true;
+            }
+        }
+        public int GetHandle()
+        {
+            return _handle;
+        }
+        public bool IsDemo() //check if currently running in demo mode (i.e. couldn't find a labjack device)
+        {
+            return _isDemo;
+        }
+        public void CloseDevice()
+        {
+            if (_handle > 0)
+            {
+                LJM.Close(_handle);
+                _handle = -1;
+            }
+        }
+    }
+}
