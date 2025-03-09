1891888adc4a18b5c34e74681ca8badc238445d3
Trey Tipton <treytipto@outlook.com>
2025-03-08 23:32:10 -0600

    Appeased the Microsoft gods
    After gutting the welcome and replay page I encountered a bug
    that made your cursor a loading circle and prevented interaction with
    the application. Bill Gates personally demanded I add a .5 second delay on a static page
    before moving to the recording page otherwise the application would not work.
    
    squash me to microsoft appeasement commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using Project_FREAK.Views;  // Imports Views namespace and allows us to change the page displayed in the MainFrame.
 using Project_FREAK.Views.Settings;  // Imports Settings namespace and allows us to change the page displayed in the SettingsFrame.
 
@@ -18,14 +19,25 @@ namespace Project_FREAK
         public MainWindow()
         {
             InitializeComponent();
-            MainFrame.Navigate(new RecordPage());   // Navigate to RecordPage on startup
+            this.Loaded += MainWindow_Loaded; // Subscribe to the Loaded event
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new LoadingPage()); // Navigate to LoadingPage first (Static page to allow for async loading of the app)
+
+            await Task.Yield(); // Yield to the dispatcher to allow the UI to update
+
+            await Dispatcher.InvokeAsync(() =>  // Make sure we appease the Microsoft gods before attempting to navigate to the RecordPage
+            {
+                MainFrame.Navigate(new RecordPage());
+            }, DispatcherPriority.Background);
         }
 
         public void NavigateToPage(Page page, string pageName)
         {
             MainFrame.Navigate(page);
         }
-
 
         private void SettingsMenuItem_Click(object sender, RoutedEventArgs e)
         {
